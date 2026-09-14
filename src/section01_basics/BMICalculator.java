e337b9b8335813da2f2deb42a371903a8b880c43
Mahmut Encu <encu.mahmut415040@gmail.com>
2026-09-14 15:30:08 +0300

    feat: update operators and finalize BMICalculator

--- src/section01_basics/BMICalculator.java
+++ src/section01_basics/BMICalculator.java
@@ -1,8 +1,24 @@
 package section01_basics;
 
+import java.util.Scanner;
+
 public class BMICalculator {
 public static void main (String[] args) {
 
+    /*Body Mass Index Calculation algorithm
+    1)initialize the Scanner object
+    2)Get weight(kg) from the user as double
+    3)get height(m) from the user as double
+    4)apply the bmı formula
+    5)print the result to the screen
+     */
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("pleas enter your weight as kg  ");
+    double weight = scanner.nextDouble();
+    System.out.println("please enter your height as a meter  ");
+    double height = scanner.nextDouble();
+    double BMI = weight / (height * height);
+    System.out.println("your body mass index is\t" + BMI);
 
 
 
--- src/section01_basics/Operators.java
+++ src/section01_basics/Operators.java
@@ -1,4 +1,41 @@
 package section01_basics;
 
 public class Operators {
+public static void main (String[] args) {
+
+    /*Operators*/
+
+/*
+(+) -->> addition
+(-) -->> subtraction
+(*) -->> multiplication
+(/) -->> division
+(%) -->> remainder
+
+            System.out.println(120+234);
+            System.out.println(350-550);
+            System.out.println(350*4);
+            System.out.println(350/4d);
+            System.out.println(350%4);
+
+ */
+//increment and decrement operators
+
+   /* int a = 5;
+    int b = 15;
+    a++;//posfix:increases the value of a by 1
+    b--; //posfix:decreases the value of b by 1
+
+    ++a;//prefix:increases the value of a by 1 with updating
+    --b;//prefix: decreases the value of b by 1 with updating
+
+
+    a +=5; // means increas the valu of a by 5
+    b -=5; // means decreas the value of b by 5
+    System.out.println(a);
+    System.out.println(b);
+*/
+
+
+}
 }
