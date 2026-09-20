6b05debb751723f29b4c1ea7b8665c1689bd8c86
Mahmut Encu <encu.mahmut415040@gmail.com>
2026-09-20 15:43:48 +0300

    feat:practice while loops and condition-based iteration

---
+++ src/section03_loops/WhileLoops.java
@@ -0,0 +1,49 @@
+package section03_loops;
+
+import java.util.Scanner;
+
+public class WhileLoops {
+    public static void main(String[] args) {
+        Scanner scaner = new Scanner(System.in);
+
+
+      /* While Loop
+
+      while (loop condition){
+      This code block runs as long as the condition is met
+      }
+       */
+     /*
+      int i = 0 ; // define  the loops variable
+
+         while (i<10){
+             System.out.println("i = "+ i);
+             i++;
+         }
+         */
+
+        /* factorial operation with while loop*/
+
+       /* System.out.println("please enter a number ");
+         int number= scaner.nextInt();
+         int factorial=1;
+         while(number>=1){
+            factorial*=number;
+            number--;
+
+
+         }
+        System.out.println(factorial);
+        */
+
+        /* Infinite Loop situation
+        If no action is taken to break the while loop's condition, the loop runs forever.*/
+
+        /*   int i = 0;
+             while (i<10){
+             System.out.println(i);
+             i--;
+        }  */
+
+    }
+}
